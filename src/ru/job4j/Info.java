c080eefdc5aad949cbd3b4a286663ff117585224
sid <1@com>
2020-10-12 21:58:22 +0500

    9.1. String [#253303]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -1,12 +1,12 @@
 package ru.job4j;
 
 public class Info {
-    public static void main(String[] args){
-        System.out.println("11.10.2020");
+    public static void main(String[] args) {
+                System.out.println("11.10.2020");
         byte mem = 9;
         String what = "test";
         char y = 'Y';
-        System.out.println(what + " " +y);
+        System.out.println(what + " " + y);
 
     }
 }
