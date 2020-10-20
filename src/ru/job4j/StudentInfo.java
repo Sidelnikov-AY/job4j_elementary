9c87aa5fcf988565b3c84dd4acc0adb33704e068
sid <1@com>
2020-10-20 23:21:15 +0500

    3.3. Разбор первой программы. [#391798]

--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -1,9 +1,8 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Anton Sidelnikov");
         System.out.println("11.10.2020");
-
     }
 }
