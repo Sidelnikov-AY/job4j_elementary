8a9ae513a5ac10a23640ce8fe8c592e2de10ffed
sid <1@com>
2020-10-23 07:58:42 +0500

    9.1. String [#391788]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -4,9 +4,9 @@ import java.sql.SQLOutput;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java";
+        String idea = "I like Java!";
         System.out.println(idea);
-        idea = idea + " " + "But I am a newbie";
+        idea = idea + " " + "But I am a newbie.";
         int year = 2020;
         idea = idea + " " + year;
         System.out.println(idea);
