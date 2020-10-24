36e41472f024062ac68d8d394bd9d4cc9f7627b5
sid <1@com>
2020-10-24 21:50:08 +0500

    1. Модульные тесты. [#391811]

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -9,6 +9,7 @@ public class ConverterTest {
 
     @Test
     public void whenConvert180RblThen3Dlr() {
+
         int in = 180;
         int expected = 3;
         int out = Converter.rubleToDollar(in);
@@ -17,6 +18,7 @@ public class ConverterTest {
 
     @Test
     public void whenConvert140RblThen2Euro() {
+
         int in = 140;
         int expected = 2;
         int out = Converter.rubleToEuro(in);
