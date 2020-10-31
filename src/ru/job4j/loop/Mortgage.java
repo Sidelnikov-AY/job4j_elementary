88b320cbecabde8c461f07f0db922e3d1ed84825
sid <1@com>
2020-10-31 18:51:36 +0500

    5.7. Ипотека [#391837]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,9 +2,10 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
-        int year = 1;
+        int year = 0;
         double mortragePercent = 1 + percent / 100;
-        while (amount * mortragePercent > salary)   {
+        while (amount > 0)   {
+            amount *= mortragePercent;
             amount = amount - salary;
             year++;
         }
