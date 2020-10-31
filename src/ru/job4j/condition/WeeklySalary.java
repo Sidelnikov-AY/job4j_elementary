75127363c68e66e748efd726b0bef748daf5012b
sid <1@com>
2020-10-31 21:33:17 +0500

    6.0.3.1. Массивы и цикл for-earch [#391769]

---
+++ src/ru/job4j/condition/WeeklySalary.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+public class WeeklySalary {
+    public static int calculate(int[] hours) {
+        int calc = 0;
+        for (int index = 0; index < hours.length; index++) {
+            //проверяем рабочие дни
+            if (index < 5) {
+                //проверяем рабочее время
+                if (hours[index] < 9) {
+                    calc += hours[index] * 10;
+
+                }
+                // проверяем нерабочее время
+                else if (hours[index] > 8) {
+
+                    calc += (8 * 10) + (hours[index] - 8) * 15;
+
+                }
+            }
+            // проверяем выходные - рабочее время
+            else if (hours[index] < 9) {
+                calc += hours[index] * 10 * 2;
+
+            }
+            // проверяем выходные - нерабочее время
+            else {
+                calc += (8 * 10 * 2) + (hours[index] - 8) * 15 * 2;
+
+            }
+        }
+        return calc;
+    }
+}
